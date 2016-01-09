167bde1b7ba6fec6c62bf292fe12add580e63d7d
Dave Kerr <davek09@gmail.com>
2016-01-09 15:33:14 -0500

    cleanup gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -6,25 +6,18 @@ gulp.task('default', ['browser-sync'], function() {});
 
 gulp.task('browser-sync', ['nodemon'], function() {
   browserSync.init(null, {
-		proxy: "http://localhost:3000",
+    proxy: "http://localhost:3000",
     files: ["public/**/*.*"],
     port: 7000,
-	});
+  });
 });
 
-
 gulp.task('nodemon', function (cb) {
-	
-	var started = false;
-	
-	return nodemon({
-		script: 'server.js'
-	}).on('start', function () {
-		// to avoid nodemon being started multiple times
-		// thanks @matthisk
-		if (!started) {
-			cb();
-			started = true; 
-		} 
-	});
+  var started = false;
+  
+  return nodemon({
+    script: 'server.js'
+  }).on('start', function () {
+    if (!started) { cb(); started = true; } 
+  });
 });
